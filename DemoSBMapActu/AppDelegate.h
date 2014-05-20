7e6da0b8ba8063a3728d90ff4dfdf97fc1b2728a
Tom Bentz <Tom@iMac.local>
2014-05-20 15:14:44 +0200

    Initial Commit

---
+++ DemoSBMapActu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoSBMapActu
+//
+//  Created by Tom Bentz on 20/05/14.
+//  Copyright (c) 2014 IPSTAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoSBMapActuTests/DemoSBMapActuTests.m
@@ -0,0 +1,34 @@
+//
+//  DemoSBMapActuTests.m
+//  DemoSBMapActuTests
+//
+//  Created by Tom Bentz on 20/05/14.
+//  Copyright (c) 2014 IPSTAND. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoSBMapActuTests : XCTestCase
+
+@end
+
+@implementation DemoSBMapActuTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
